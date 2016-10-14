49a9b9ec2ce51a5adf3f7f2dd5de30bcef7cc528
Stoyan Stoyanov <stoyan.stoyanov@sstoyanov-imac.local>
2016-10-14 15:06:33 +0300

    basic game mechanics implemented

--- SpriteKitDemo/GameScene.m
+++ SpriteKitDemo/GameScene.m
@@ -8,72 +8,76 @@
 
 #import "GameScene.h"
 
+@interface GameScene()
+
+@property (nonatomic, strong) SKSpriteNode *backgroundNode;
+@property (strong, nonatomic) SKSpriteNode *player;
+@property (strong, nonatomic) SKSpriteNode *swipeControl;
+@property (strong, nonatomic) SKSpriteNode *swipeIcon;
+@property (strong, nonatomic) SKLabelNode *swipeStartLabel;
+
+@end
+
 @implementation GameScene {
-    SKShapeNode *_spinnyNode;
-    SKLabelNode *_label;
+    BOOL __block _gameIsStarted;
 }
+
+#pragma mark - Scene Setup
 
 - (void)didMoveToView:(SKView *)view {
-    // Setup your scene here
-    
-    // Get label node from scene and store it for use later
-    _label = (SKLabelNode *)[self childNodeWithName:@"//helloLabel"];
-    
-    _label.alpha = 0.0;
-    [_label runAction:[SKAction fadeInWithDuration:2.0]];
-    
-    CGFloat w = (self.size.width + self.size.height) * 0.05;
-    
-    // Create shape node to use during mouse interaction
-    _spinnyNode = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(w, w) cornerRadius:w * 0.3];
-    _spinnyNode.lineWidth = 2.5;
-    
-    [_spinnyNode runAction:[SKAction repeatActionForever:[SKAction rotateByAngle:M_PI duration:1]]];
-    [_spinnyNode runAction:[SKAction sequence:@[
-                                                [SKAction waitForDuration:0.5],
-                                                [SKAction fadeOutWithDuration:0.5],
-                                                [SKAction removeFromParent],
-                                                ]]];
+    self.backgroundNode = (SKSpriteNode *)[self childNodeWithName:@"scrollingBackground"];
+    self.player = (SKSpriteNode *)[self childNodeWithName:@"player"];
+    self.swipeControl = (SKSpriteNode *)[self childNodeWithName:@"swipeControl"];
+    self.swipeIcon = (SKSpriteNode *)[self.swipeControl childNodeWithName:@"swipeIcon"];
+    self.swipeStartLabel = (SKLabelNode *)[self.swipeControl childNodeWithName:@"gameStart"];
 }
 
+#pragma mark - Touch Handling
 
-- (void)touchDownAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor greenColor];
-    [self addChild:n];
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if ([self isTouchInSwipeControl:touches.anyObject]) {
+        [self startLevel];
+    }
 }
 
-- (void)touchMovedToPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor blueColor];
-    [self addChild:n];
-}
-
-- (void)touchUpAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor redColor];
-    [self addChild:n];
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    // Run 'Pulse' action from 'Actions.sks'
-    [_label runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
-    for (UITouch *t in touches) {[self touchDownAtPoint:[t locationInNode:self]];}
-}
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    for (UITouch *t in touches) {[self touchMovedToPoint:[t locationInNode:self]];}
-}
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *t in touches) {[self touchUpAtPoint:[t locationInNode:self]];}
-}
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *t in touches) {[self touchUpAtPoint:[t locationInNode:self]];}
+    if ([self isTouchInSwipeControl:touches.anyObject] && _gameIsStarted) {
+        UITouch *singleTouch = touches.anyObject;
+        CGPoint coordinates = [singleTouch locationInNode:self];
+        SKAction *moveAction = [SKAction moveToX:coordinates.x duration:0.1];
+        [self.player runAction:moveAction];
+    }
 }
 
+#pragma mark - Helper Methods
+
+- (void)startLevel {
+    
+    SKAction *waitAction = [SKAction waitForDuration:2];
+    SKAction *moveDownAction = [SKAction moveToY:1540 duration:10];
+    [self.backgroundNode runAction:[SKAction sequence:@[waitAction, moveDownAction]]];
+    
+    GameScene __weak *weakSelf = self;
+    [self.player runAction:waitAction completion:^{
+        weakSelf.player.physicsBody.affectedByGravity = YES;
+        _gameIsStarted = YES;
+    }];
+    
+    SKAction *fadeOut = [SKAction fadeOutWithDuration:1];
+    [self.swipeStartLabel runAction:fadeOut];
+    
+    SKAction *fadeIn = [SKAction fadeInWithDuration:1];
+    [self.swipeIcon runAction:fadeIn];
+}
+
+- (BOOL)isTouchInSwipeControl:(UITouch *)touch {
+    CGPoint locationInScene = [touch locationInNode:self];
+    SKNode *touchedNode = [self nodeAtPoint:locationInScene];
+    return [touchedNode.parent isEqualToNode:self.swipeControl] || [touchedNode isEqualToNode:self.swipeControl];
+}
+
+#pragma mark - Frame Update Callback
 
 -(void)update:(CFTimeInterval)currentTime {
     // Called before each frame is rendered
