c928662f2a43247f0c27401fa8175e580f1b25bf
Stoyan Stoyanov <stoyan.stoyanov@sstoyanov-imac.local>
2016-10-27 18:11:32 +0300

    returned back app launch to introscreen

--- SpriteKitDemo/GameViewController.m
+++ SpriteKitDemo/GameViewController.m
@@ -16,7 +16,7 @@
     [super viewDidLoad];
 
     [[GameManager sharedManager] setSpriteKitView:(SKView *)self.view];
-    [[GameManager sharedManager] loadMainMenuSceneWithEntranceAnimationsEnabled:YES];
+    [[GameManager sharedManager] loadIntroScene];
 }
 
 - (BOOL)shouldAutorotate {
